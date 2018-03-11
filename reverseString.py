686730f0a710d396cb9480d948febe9c6e60c5e1
pinkal1 <pinkal.patel1991@gmail.com>
2018-03-11 14:14:50 -0700

    Add files via upload

--- reverseString.py
+++ reverseString.py
@@ -14,5 +14,51 @@ print reverse(string)'''
 def reverse(string):
     return string[::-1]
 
-string=raw_input("Please enter string:")
-print reverse(string)
+string=input("Please enter string:")
+print (reverse(string))
+
+#reverse string
+
+a='hello world'[::-1]
+print(a)
+
+b='hello world'
+print(''.join(reversed(b)))
+
+def reverse(s):
+    r = ""
+    for c in s:
+        r= c+r
+    return r
+s="hello world"
+print(reverse(s))
+
+
+
+
+# Reverse list
+
+os = ['Windows', "macOS" , "Linux"]
+print('Original list:',os)
+
+os.reverse()
+print('Reversed List',os)
+
+os = ['Windows', "macOS" , "Linux"]
+rev=os[::-1]
+print('Reversed List',rev)
+
+os = ['Windows', "macOS" , "Linux"]
+for o in reversed(os):
+    print(o)
+
+list_data= ['Windows', "macOS" , "Linux"]
+#print(len(list_data))
+l = len(list_data)
+i=l+1
+rev_data = []
+while l>0:
+  j=i-l
+  l-=1
+  rev_data.append(list_data[-j])
+print ("After Rev:- %s" %rev_data)
