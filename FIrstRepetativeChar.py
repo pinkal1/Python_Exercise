d79c9bb8c5a7a269705c63aa563f94ead0848365
pinkal1 <pinkal.patel1991@gmail.com>
2018-03-11 11:20:38 -0700

    first_repeating+most_consecutive_char
    first_repeating+most_consecutive_char

--- FIrstRepetativeChar.py
+++ FIrstRepetativeChar.py
@@ -2,32 +2,41 @@
 # Example: input: 123LK63824K output: 3
 
 
+def first_repeating_char(string):
+    d={}
+    for char in string:
+        if char in d.keys():
+            print ("First repeating character is :",char)
+            break
+        else:
+            d[char] =0
+
 string='123LK63824K'
-d={}
-for char in string:
-    if char in d.keys():
-        print char
-        break
-    else:
-        d[char] =0
+print ("string is: ",string)
+first_repeating_char(string)
 
-print "----------------------------------------"
+print ("----------------------------------------")
 
-# example: word="aaaabbcccaahhhhhhah"   op h: 6   not h:7   hould be consecutive char
+# example: word="aaaabbcccaahhhhhhah"   op h: 6   not h:7   should be consecutive char
 
-word = "aaaabbcccaahhhhhhah"
-count = 1
-length = dict()
-for i in range(1, len(word)):
-    if word[i - 1] == word[i]:
-        count += 1
-    else:
-        length[str(count)] = word[i - 1]
-        count = 1
-length[str(count)] = word[i]
-print (length)
+def most_consecutive_char(string):
+    count = 1
+    d = {}
+    for i in range(1, len(string)):
+        if string[i - 1] == string[i]:
+            count += 1
+        else:
+            d[str(count)] = string[i - 1]
+            count = 1
+    d[str(count)] = string[i]
+    print (d)
+    print (max(d.keys()), d[max(d.keys())])
+    print(d[max(d.keys())],'occurs', max(d.keys()),'times')
 
-print max(length.keys()), length[max(length.keys())]
+string = "aaaabbcccaahhhhhhah"
+print ("string is: ",string)
+most_consecutive_char(string)
+
 
 
 
